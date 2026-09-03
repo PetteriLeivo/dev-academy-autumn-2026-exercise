22b0df980b07ddd53b6c24d834d5ecbce022fb5b
PetteriLeivo <49028429+PetteriLeivo@users.noreply.github.com>
2026-09-03 17:29:23 +0300

    switch folder from previous commit changes

--- Backend/src/sql/db.ts
+++ Backend/src/sql/db.ts
@@ -1,7 +1,11 @@
 import pgPromise from 'pg-promise';
-const pgp = pgPromise({});
+
+const pgp = pgPromise({})
+
 // Database connection configuration
 const connectionString = 'postgres://academy:academy@localhost:5432/electricity';
+
 // Create the database instance
 const db = pgp(connectionString);
+
 export { db, pgp };

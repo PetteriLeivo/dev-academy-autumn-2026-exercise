d73b21a0e2199902fdf02929e67a56403fa44a20
PetteriLeivo <49028429+PetteriLeivo@users.noreply.github.com>
2026-09-09 20:53:28 +0300

    fix(backend): import dotenv

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import 'dotenv/config'
 
 const frontendDistPath = path.join(import.meta.dirname, '../../Frontend/electricity_info/dist');
 
